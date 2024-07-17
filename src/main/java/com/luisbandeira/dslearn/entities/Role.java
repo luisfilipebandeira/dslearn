f95b2794dcd43862bf4a3840edb9e25249a6876a
Luis Bandeira <luisbandeira@brq.com>
2024-07-17 18:34:20 -0300

    updates

--- src/main/java/com/luisbandeira/dslearn/entities/Role.java
+++ src/main/java/com/luisbandeira/dslearn/entities/Role.java
@@ -16,10 +16,9 @@ public class Role implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String authority;
 
-    public Role() {
+    public Role(){
     }
 
     public Role(Long id, String authority) {
@@ -44,24 +43,28 @@ public class Role implements Serializable {
         this.authority = authority;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
     public boolean equals(Object obj) {
-        if (this == obj) {
+        if (this == obj)
             return true;
-        }
-        if (obj == null) {
+        if (obj == null)
             return false;
-        }
-        if (getClass() != obj.getClass()) {
+        if (getClass() != obj.getClass())
             return false;
-        }
         Role other = (Role) obj;
         if (id == null) {
-            if (other.id != null) {
+            if (other.id != null)
                 return false;
-            }
-        } else if (!id.equals(other.id)) {
+        } else if (!id.equals(other.id))
             return false;
-        }
         return true;
     }
 }
--- src/main/java/com/luisbandeira/dslearn/entities/User.java
+++ src/main/java/com/luisbandeira/dslearn/entities/User.java
@@ -16,7 +16,7 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_user")
-public class User implements Serializable{
+public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
@@ -82,24 +82,29 @@ public class User implements Serializable{
         this.roles = roles;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
     public boolean equals(Object obj) {
-        if (this == obj) {
+        if (this == obj)
             return true;
-        }
-        if (obj == null) {
+        if (obj == null)
             return false;
-        }
-        if (getClass() != obj.getClass()) {
+        if (getClass() != obj.getClass())
             return false;
-        }
         User other = (User) obj;
         if (id == null) {
-            if (other.id != null) {
+            if (other.id != null)
                 return false;
-            }
-        } else if (!id.equals(other.id)) {
+        } else if (!id.equals(other.id))
             return false;
-        }
         return true;
     }
+
 }
