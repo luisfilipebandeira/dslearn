eb3884160a79f6c4a02776c8dbe59d2612d2eaad
Luis Bandeira <luisbandeira@brq.com>
2024-07-25 16:35:06 -0300

    adding section class

--- src/main/java/com/luisbandeira/dslearn/entities/Section.java
+++ src/main/java/com/luisbandeira/dslearn/entities/Section.java
@@ -1,8 +1,6 @@
 package com.luisbandeira.dslearn.entities;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,46 +8,42 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-import com.luisbandeira.dslearn.enums.ResourceType;
-
 @Entity
-@Table(name = "tb_resource")
-public class Resource implements Serializable {
+@Table(name = "tb_section")
+public class Section implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String title;
     private String description;
     private Integer position;
     private String imgUri;
-    private ResourceType type;
 
     @ManyToOne
-    @JoinColumn(name = "offer_id")
-    private Offer offer;
+    @JoinColumn(name = "resource_id")
+    private Resource resource;
 
-    @OneToMany(mappedBy = "resource")
-    private List<Section> sections = new ArrayList<>();
+    @ManyToOne
+    @JoinColumn(name = "prerequisite_id")
+    private Section prerequisite;
 
-    public Resource() {
+    public Section() {
     }
 
-    public Resource(Long id, String title, String description, Integer position, String imgUri, ResourceType type,
-            String externalLink, Offer offer) {
+    public Section(Long id, String title, Integer position, String description, String imgUri, Resource resource,
+            Section prerequisite) {
         super();
         this.id = id;
         this.title = title;
-        this.description = description;
         this.position = position;
+        this.description = description;
         this.imgUri = imgUri;
-        this.type = type;
-        this.offer = offer;
+        this.resource = resource;
+        this.prerequisite = prerequisite;
     }
 
     public Long getId() {
@@ -64,56 +58,48 @@ public class Resource implements Serializable {
         return title;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
     public Integer getPosition() {
         return position;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public void setPosition(Integer position) {
         this.position = position;
     }
 
+    public String getDescription() {
+        return description;
+    }
+
     public String getImgUri() {
         return imgUri;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public void setImgUri(String imgUri) {
         this.imgUri = imgUri;
     }
 
-    public ResourceType getType() {
-        return type;
+    public Resource getResource() {
+        return resource;
     }
 
-    public void setType(ResourceType type) {
-        this.type = type;
+    public void setResource(Resource resource) {
+        this.resource = resource;
     }
 
-    public Offer getOffer() {
-        return offer;
+    public Section getPrerequisite() {
+        return prerequisite;
     }
 
-    public void setOffer(Offer offer) {
-        this.offer = offer;
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        return result;
+    public void setPrerequisite(Section prerequisite) {
+        this.prerequisite = prerequisite;
     }
 
     @Override
@@ -124,12 +110,20 @@ public class Resource implements Serializable {
             return false;
         if (getClass() != obj.getClass())
             return false;
-        Resource   other = (Resource) obj;
+        Section other = (Section) obj;
         if (id == null) {
             if (other.id != null)
                 return false;
         } else if (!id.equals(other.id))
             return false;
         return true;
+    }
+    
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
     }
 }
